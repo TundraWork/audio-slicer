be8d554ec3dbc583b6f17168809ea7d7b3618aa3
Tundra Work <tundrawork@gmail.com>
2023-12-10 08:28:59 +1100

    chore: drop pyqtdarktheme and darkdetect

--- slicer-gui.py
+++ slicer-gui.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import datetime
-import qdarktheme
 from PySide6.QtWidgets import QApplication, QStyleFactory
 from PySide6.QtGui import QFont
 
@@ -19,19 +18,6 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setApplicationName("Audio Slicer")
     app.setApplicationDisplayName("Audio Slicer")
-    
-    # Apply auto dark theme
-    qdarktheme.setup_theme(
-        theme="auto",
-        # custom_colors={
-        #     "[dark]": {
-        #         "primary": "#8dc8d1",
-        #         },
-        #     "[light]": {
-        #         "primary": "#3b7d92",
-        #     }
-        # }
-    )
 
     # Auto dark title bar on Windows 10/11
     style = QStyleFactory.create("fusion")
